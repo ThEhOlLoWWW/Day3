0052c04a629e02a68842a4d787c839638b26aad3
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-10 16:05:02 +0100

    ex11 done

--- exercices/exercice-11.js
+++ exercices/exercice-11.js
@@ -14,4 +14,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+function calculerFactorielle(n){
+    if ( n === 0 || n === 1){
+        return 1
+    }
+        return n * calculerFactorielle(n - 1);
+}
+
+console.log(calculerFactorielle(0));
+
