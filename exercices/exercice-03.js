f18c72ebd42b8ab23e7a938c195d5ccb2ed4658e
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-10 14:56:07 +0100

    ex3 done

--- exercices/exercice-03.js
+++ exercices/exercice-03.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+function madToEur(mad) {
+    let euros = mad / 10.8;
+    return euros;
+}
+console.log(madToEur(21.6));
