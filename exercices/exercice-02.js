420d54ac06bf09bda160e44f73c5de445b51895b
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-10 14:52:46 +0100

    ex3 done

--- exercices/exercice-02.js
+++ exercices/exercice-02.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+ let mult = (a,b) => {
+    return a * b;
+ }
+
+ console.log(mult(5,2));
+ 
